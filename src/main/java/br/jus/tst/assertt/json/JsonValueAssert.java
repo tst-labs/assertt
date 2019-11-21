99284a466ac5bc470be47f85e059cc8f25769e54
Jean Clay Souza da Silva <jean.clay@tst.jus.br>
2019-11-21 13:58:22 -0300

    qulice

--- src/main/java/br/jus/tst/assertt/json/JsonValueAssert.java
+++ src/main/java/br/jus/tst/assertt/json/JsonValueAssert.java
@@ -21,9 +21,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package br.jus.tst.assertt.json;
 
-import static javax.json.JsonValue.ValueType;
+package br.jus.tst.assertt.json;
 
 import java.util.EnumMap;
 import java.util.ListIterator;
@@ -35,12 +34,18 @@ import org.assertj.core.api.AbstractAssert;
 
 /**
  * Custom assertions for <code>javax.json.JsonValue</code>.
+ * @since 0.1
  */
 public class JsonValueAssert extends AbstractAssert<JsonValueAssert, JsonValue> {
 
-    private static final EnumMap<ValueType, BiFunction<JsonValue, JsonValue, Boolean>> ACTIONS = new EnumMap<>(
-        ValueType.class
-    );
+    /**
+     * Uses EnumMap instead of 'switch case' to decide which comparisson object
+     * will be used.
+     */
+    private static final EnumMap
+        <JsonValue.ValueType,
+        BiFunction<JsonValue, JsonValue, Boolean>
+        > ACTIONS = new EnumMap<>(JsonValue.ValueType.class);
 
     static {
         ACTIONS.put(JsonValue.ValueType.ARRAY, new CompareArrays());
@@ -52,34 +57,71 @@ public class JsonValueAssert extends AbstractAssert<JsonValueAssert, JsonValue> 
         ACTIONS.put(JsonValue.ValueType.TRUE, new ComparePrimitives());
     }
 
+    /**
+     * Main constructor.
+     * @param actual The value to be compared against the expected value.
+     */
     public JsonValueAssert(final JsonValue actual) {
         super(actual, JsonValueAssert.class);
     }
 
+    /**
+     * Entry point. Follows AssertJ idiom.
+     * @param actual The value to be compared against the expected value.
+     * @return This instance.
+     */
     @SuppressWarnings("PMD.ProhibitPublicStaticMethods")
     public static JsonValueAssert assertThat(final JsonValue actual) {
         return new JsonValueAssert(actual);
     }
 
+    /**
+     * Verifies that the expected value is equal to the actual value.
+     * @param expected Expected value.
+     * @return Boolean.
+     */
     public JsonValueAssert isEqualTo(final JsonValue expected) {
-        if (!areEqual(expected)) {
-            failWithMessage("Esperava-se que o valor fosse <%s>, mas foi <%s>", expected, actual);
+        if (!this.areEqual(expected)) {
+            failWithMessage(
+                "Esperava-se que o valor fosse <%s>, mas foi <%s>",
+                expected,
+                actual
+            );
         }
         return this;
     }
 
+    /**
+     * Verifies that the expected value is not equal to the actual value.
+     * @param expected Expected value.
+     * @return Boolean.
+     */
     public JsonValueAssert isNotEqualTo(final JsonValue expected) {
-        if (areEqual(expected)) {
-            failWithMessage("Esperava-se que o valor <%s> fosse diferente de <%s>", expected, actual);
+        if (this.areEqual(expected)) {
+            failWithMessage(
+                "Esperava-se que o valor <%s> fosse diferente de <%s>",
+                expected,
+                actual
+            );
         }
         return this;
     }
 
+    /**
+     * Verifies if the expected value and the actual value are equal.
+     * @param expected EXpected value.
+     * @return Boolean.
+     */
     private Boolean areEqual(final JsonValue expected) {
         isNotNull();
-        return ACTIONS.get(actual.getValueType()).apply(actual, expected);
+        return ACTIONS.get(this.actual.getValueType())
+            .apply(this.actual, expected);
     }
 
+    /**
+     * Compares primitive values.
+     * @since 0.1
+     */
     private static class ComparePrimitives extends CompareJsonValues {
 
         @Override
@@ -89,20 +131,25 @@ public class JsonValueAssert extends AbstractAssert<JsonValueAssert, JsonValue> 
 
     }
 
+    /**
+     * Compares array values in the same order they appear.
+     * @since 0.1
+     */
     private static class CompareArrays extends CompareJsonValues {
 
         @Override
         protected Boolean compare(final JsonValue first, final JsonValue second) {
             final JsonArray array = (JsonArray) first;
             final JsonArray other = (JsonArray) second;
-            boolean result;
+            final boolean result;
             if (array.size() == other.size()) {
                 final ListIterator<JsonValue> iterator = array.listIterator();
                 boolean equals = true;
                 while (iterator.hasNext()) {
                     final int index = iterator.nextIndex();
                     final JsonValue next = iterator.next();
-                    equals = equals && ACTIONS.get(next.getValueType()).apply(next, other.get(index));
+                    equals = equals && ACTIONS.get(next.getValueType())
+                        .apply(next, other.get(index));
                 }
                 result = equals;
             } else {
@@ -112,13 +159,17 @@ public class JsonValueAssert extends AbstractAssert<JsonValueAssert, JsonValue> 
         }
     }
 
+    /**
+     * Makes a deep comparisson.
+     * @since 0.1
+     */
     private static class CompareObjects extends CompareJsonValues {
 
         @Override
         protected Boolean compare(final JsonValue first, final JsonValue second) {
             final JsonObject object = (JsonObject) first;
             final JsonObject other = (JsonObject) second;
-            boolean result;
+            final boolean result;
             if (object.entrySet().size() == other.entrySet().size()) {
                 result = object.entrySet()
                     .stream()
@@ -136,6 +187,10 @@ public class JsonValueAssert extends AbstractAssert<JsonValueAssert, JsonValue> 
 
     }
 
+    /**
+     * Compare null values.
+     * @since 0.1
+     */
     private static class CompareNulls extends CompareJsonValues {
 
         @Override
@@ -144,7 +199,12 @@ public class JsonValueAssert extends AbstractAssert<JsonValueAssert, JsonValue> 
         }
     }
 
-    private abstract static class CompareJsonValues implements BiFunction<JsonValue, JsonValue, Boolean> {
+    /**
+     * Verifies if two json values are equal.
+     * @since 0.1
+     */
+    private abstract static class CompareJsonValues
+        implements BiFunction<JsonValue, JsonValue, Boolean> {
 
         @Override
         public final Boolean apply(final JsonValue first, final JsonValue second) {
--- src/main/java/br/jus/tst/assertt/json/package-info.java
+++ src/main/java/br/jus/tst/assertt/json/package-info.java
@@ -21,4 +21,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
+/**
+ * Assertions on JsonValue.
+ */
 package br.jus.tst.assertt.json;
